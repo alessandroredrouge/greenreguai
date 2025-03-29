78541263b066d3e539697b1e27cce65082aecec3
alessandroredrouge <alessandroredrouge@gmail.com>
2025-03-29 12:22:21 +0100

    new update

--- backend/main.py
+++ backend/main.py
@@ -44,14 +44,16 @@ app.add_middleware(
         "https://greenreguai.com",
         "https://www.greenreguai.com",
         "https://nothingness-narrative.lovable.app",
-        "https://greenreguaichallenge.lovable.app/"
+        "https://greenreguaichallenge.lovable.app/",
+        "https://preview--greenreguaichallenge.lovable.app"
     ] if settings.ENV == "development" else [
         "https://greenreguai.onrender.com",
         "https://greenreguai-api.onrender.com",
         "https://greenreguai.com",
         "https://www.greenreguai.com",
         "https://nothingness-narrative.lovable.app",
-        "https://greenreguaichallenge.lovable.app/"
+        "https://greenreguaichallenge.lovable.app/",
+        "https://preview--greenreguaichallenge.lovable.app"
     ],
     allow_credentials=True,
     allow_methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"],
